cbd7c66bdd2775a739dde562bdc2eba53a6c1dfb
Mike Splain <mike.splain@barkly.com>
2017-01-02 17:55:55 -0500

    Add repos and projects

--- projects/base/setup.sh
+++ projects/base/setup.sh
@@ -4,6 +4,19 @@ BASEDIRFILE=$(dirname $0)
 cd $BASEDIRFILE
 BASEDIR=$(pwd -P)
 
+if ! [ $BARKLYDIR ]; then
+  echo Barkly directroy not defined.
+  echo Please run from brew barkly instead.
+  exit 1
+fi
+
 echo "Base setup."
 echo "Starting brew installation..."
+echo
 brew bundle --file="$BASEDIR/Brewfile"
+echo
+
+echo "Cloning repos"
+while read REPO; do
+  echo ">> Cloning $REPO into $BARKLYDIR/$REPO"
+done < $BASEDIR/repos
