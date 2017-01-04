97beb83a02423b7212bac9c974db39007813285f
Mike Splain <mike.splain@barkly.com>
2017-01-04 13:08:01 -0500

    Add mikesplain user

---
+++ users/mikesplain/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+THISDIRFILE=$(dirname $0)
+cd $THISDIRFILE
+THISDIR=$(pwd -P)
+. "$HOMEBREWBARKLYDIR/lib/init.sh"
+
+# Start your script below and user any helpsers setup in lib/init.sh
