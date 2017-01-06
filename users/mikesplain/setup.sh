df69b8d0c1c1c72063307840d9cb391d7da22d55
Mike Splain <mike.splain@barkly.com>
2017-01-06 10:26:03 -0500

    Add splain file

--- users/mikesplain/setup.sh
+++ users/mikesplain/setup.sh
@@ -5,3 +5,15 @@ THISDIR=$(pwd -P)
 . "$HOMEBREWBARKLYDIR/lib/init.sh"
 
 # Start your script below and user any helpsers setup in lib/init.sh
+
+
+cat << 'EOF' > "$HOME/.env/splain.sh"
+alias prod1='kubectl config use-context prod1 && cd ~/cylent/barkly-environments/prod1'
+alias prod2='kubectl config use-context prod2 && cd ~/cylent/barkly-environments/prod2'
+alias dev1='kubectl config use-context dev1 && cd ~/cylent/barkly-environments/dev1'
+alias dev2='kubectl config use-context dev2 && cd ~/cylent/barkly-environments/dev2'
+alias stage1='kubectl config use-context stage1 && cd ~/cylent/barkly-environments/stage1'
+alias mgmt2='kubectl config use-context mgmt2 && cd ~/cylent/barkly-environments/mgmt2/'
+alias researchkube1='kubectl config use-context researchkube1 && cd ~/cylent/barkly-environments/researchkube1/'
+alias scale_up_mgmt2='aws autoscaling set-desired-capacity --auto-scaling-group-name mgmt2_minion_spot --desired-capacity 2'
+EOF
