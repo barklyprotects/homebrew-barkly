4cc62809c30339c7ef9d3c460d108d1dc24c5108
Mike Splain <mike.splain@barkly.com>
2017-01-10 11:55:14 -0500

    Add sudoers config

--- projects/ops/setup.sh
+++ projects/ops/setup.sh
@@ -7,3 +7,10 @@ THISDIR=$(pwd -P)
 
 pip install awscli
 aws configure set default.s3.signature_version s3v4
+
+cat << EOF > /etc/sudoers.d/vagrant-syncedfolders
+Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
+Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
+Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
+%admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
+EOF
