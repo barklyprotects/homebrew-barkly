99d4e8fc82e691779db95863a0141cc56aa66c34
Mike Splain <mike.splain@barkly.com>
2017-01-10 12:59:03 -0500

    Add saws to ops

--- projects/ops/setup.sh
+++ projects/ops/setup.sh
@@ -5,12 +5,12 @@ cd $THISDIRFILE
 THISDIR=$(pwd -P)
 . "$HOMEBREWBARKLYDIR/lib/init.sh"
 
-pip install awscli
+pip install awscli saws
 aws configure set default.s3.signature_version s3v4
 
-cat << EOF > /etc/sudoers.d/vagrant-syncedfolders
+sudo bash -c 'cat << EOF > /etc/sudoers.d/vagrant-syncedfolders
 Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
 Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
 Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
 %admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
-EOF
+EOF'
