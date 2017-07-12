a3344710a7048a39d6de8057d4ae26d736f0c226
Mike Splain <mike.splain@gmail.com>
2017-07-12 10:05:31 -0400

    Update mikesplain envs

--- users/mikesplain/setup.sh
+++ users/mikesplain/setup.sh
@@ -8,12 +8,9 @@ THISDIR=$(pwd -P)
 
 
 cat << 'EOF' > "$HOME/.env/splain.sh"
-alias prod1='kubectl config use-context prod1 && cd ~/cylent/barkly-environments/prod1'
-alias prod2='kubectl config use-context prod2 && cd ~/cylent/barkly-environments/prod2'
-alias dev1='kubectl config use-context dev1 && cd ~/cylent/barkly-environments/dev1'
-alias dev2='kubectl config use-context dev2 && cd ~/cylent/barkly-environments/dev2'
-alias stage1='kubectl config use-context stage1 && cd ~/cylent/barkly-environments/stage1'
-alias mgmt2='kubectl config use-context mgmt2 && cd ~/cylent/barkly-environments/mgmt2/'
-alias researchkube1='kubectl config use-context researchkube1 && cd ~/cylent/barkly-environments/researchkube1/'
-alias scale_up_mgmt2='aws autoscaling set-desired-capacity --auto-scaling-group-name mgmt2_minion_spot --desired-capacity 2'
+alias prod2='kubectl config use-context prod2.barkly.com'
+alias dev1='kubectl config use-context dev1.barkly.com'
+alias dev2='kubectl config use-context dev2.barkly.com'
+alias mgmt1='kubectl config use-context mgmt1.barkly.com'
+alias researchkube1='kubectl config use-context researchkube1'
 EOF
