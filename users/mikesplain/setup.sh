8a810e758f0b4b8adaea7e63fa200a3abf54f8cd
Mike Splain <mike.splain@gmail.com>
2017-07-12 13:59:04 -0400

    Update dev3

--- users/mikesplain/setup.sh
+++ users/mikesplain/setup.sh
@@ -11,7 +11,7 @@ cat << 'EOF' > "$HOME/.env/splain.sh"
 alias prod2='kubectl config use-context prod2.barkly.com'
 alias dev1='kubectl config use-context dev1.barkly.com'
 alias dev2='kubectl config use-context dev2.barkly.com'
-alias dev3='kubectl config use-context dev2.barkly.com'
+alias dev3='kubectl config use-context dev3.barkly.com'
 alias mgmt1='kubectl config use-context mgmt1.barkly.com'
 alias researchkube1='kubectl config use-context researchkube1'
 alias git-saas='~/cylent/dev-tools/master-push/git-all.sh ~/cylent/dev-tools/master-push/saasRepos.txt $@'
