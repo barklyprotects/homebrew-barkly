c6546458994344c250b0fcc324286175c334d10b
Alan Yang <alan.yang@barkly.com>
2017-07-24 10:55:12 -0400

    Install grunt globally

--- projects/rml-datamart/setup.sh
+++ projects/rml-datamart/setup.sh
@@ -5,5 +5,5 @@ THISDIR=$(pwd -P)
 . "$HOMEBREWBARKLYDIR/lib/init.sh"
 
 # Start your script below and user any helpsers setup in lib/init.sh
-npm install grunt-cli
+npm install -g grunt-cli
 pip install localstack
