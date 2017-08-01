0ffa2b33203a0b935c295fecbf155bc44941099c
Mike Splain <mike.splain@gmail.com>
2017-08-01 15:44:49 -0400

    Add aliases

--- projects/kubernetes/files/env/kops.sh
+++ projects/kubernetes/files/env/kops.sh
@@ -4,3 +4,11 @@ export KOPS_STATE_STORE=s3://barkly-kops
 source <(kubectl completion zsh)
 source <(kops completion zsh)
 source <(helm completion zsh)
+
+alias prod2='kubectl config use-context prod2.barkly.com'
+alias dev1='kubectl config use-context dev1.barkly.com'
+alias dev2='kubectl config use-context dev2.barkly.com'
+alias dev3='kubectl config use-context dev3.barkly.com'
+alias mgmt1='kubectl config use-context mgmt1.barkly.com'
+alias researchkube1='kubectl config use-context researchkube1'
+alias researchkube2='kubectl config use-context researchkube2.barkly.com'
